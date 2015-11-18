14e1b0d34df3538e3d591d5faf342c75cb4e6bf2
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2015-11-18 22:16:12 +0400

    Thank you page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,51 @@
 $(function() {
 
-	$('.mc-form').ajaxChimp({
+	var $mcForm = $('.mc-form');
+	if ($mcForm.length) $mcForm.ajaxChimp({
 		url: '//movitos.us4.list-manage.com/subscribe/post-json?u=3bc400c69874a5f5dce5bfbe2&amp;id=61abe68bdf',
 		callback: function(resp) {
 			if (resp.result === 'success') {
-				window.location='thank-you.html';
+				window.location = 'thank-you.html';
 			}
 		}
 	});
 
 });
+
+// If there is a twitter button on page
+if ($('.twitter-event').length) {
+
+	// then load twitter library
+	window.twttr = (function(d, s, id) {
+		var js, fjs = d.getElementsByTagName(s)[0],
+		t = window.twttr || {};
+		if (d.getElementById(id)) return t;
+		js = d.createElement(s);
+		js.id = id;
+		js.src = "https://platform.twitter.com/widgets.js";
+		fjs.parentNode.insertBefore(js, fjs);
+
+		t._e = [];
+		t.ready = function(f) {
+			t._e.push(f);
+		};
+
+		return t;
+	}(document, "script", "twitter-wjs"));
+
+	// and bind twitter events when loaded
+	twttr.ready(function() {
+		twttr.events.bind(
+			'tweet',
+			function (event) { 
+				console.log(event);
+				var redirect = $(event.target).data('redirect');
+				if (redirect) {
+					window.location = redirect;
+				}
+			}
+		);
+	});
+
+}
+
