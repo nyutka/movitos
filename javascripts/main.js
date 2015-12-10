d3f33851e90975930d4e62cb75dd3ed8435f375d
Anne Tsugunyan <anne.tsugunyan@gmail.com>
2015-12-11 00:23:06 +0400

    Required function

--- javascripts/main.js
+++ javascripts/main.js
@@ -49,56 +49,64 @@ if ($('.twitter-event').length) {
 
 }
 
-// Mailchimp contac form
-$("#contactForm").submit(function(e) {
-	e.preventDefault();
+if ($('#contactForm').length) {
+	var $form = $('#contactForm');	
 
-	var $that = $(this);
+	$form.validatr();
 
-	var $name = $that.find('[name=name]'),
-		$email = $that.find('[name=email]'),
-		$subject = $that.find('[name=subject]'),
-		$message = $that.find('[name=message]');
+	// Mailchimp contac form
+	$form.submit(function(e) {
+		e.preventDefault();
 
-	var $button = $that.find('#msg button');
+		if (!$.validatr.validateForm($form)) return;
 
-	$button.addClass('loading').prop('disabled', true);
+		var $that = $(this);
 
-	$.ajax({
-		type: "POST",
-		url: "https://mandrillapp.com/api/1.0/messages/send.json",
-		data: {
-			'key': 'o1ycFk7uJPaLiXGX65hN0Q',
-			'message': {
-				'from_email': $email.val(),
-				'from_name': $name.val(),
-				'headers': {
-					'Reply-To': $email.val()
-				},
-				'subject': $subject.val(),
-				'text': $message.val(),
-				'to': [
-					{
-						'email': 'hellothere@wordit.com',
-						'name': 'Wordit',
-						'type': 'to'
-					}
-				]
+		var $name = $that.find('[name=name]'),
+			$email = $that.find('[name=email]'),
+			$subject = $that.find('[name=subject]'),
+			$message = $that.find('[name=message]');
+
+		var $button = $that.find('#msg button');
+
+		$button.addClass('loading').prop('disabled', true);
+
+		$.ajax({
+			type: "POST",
+			url: "https://mandrillapp.com/api/1.0/messages/send.json",
+			data: {
+				'key': 'o1ycFk7uJPaLiXGX65hN0Q',
+				'message': {
+					'from_email': $email.val(),
+					'from_name': $name.val(),
+					'headers': {
+						'Reply-To': $email.val()
+					},
+					'subject': $subject.val(),
+					'text': $message.val(),
+					'to': [
+						{
+							'email': 'hellothere@wordit.com',
+							'name': 'Wordit',
+							'type': 'to'
+						}
+					]
+				}
 			}
-		}
-	})
-	.done(function(response) {
-		$button.removeClass('loading').prop('disabled', false);
-		$name.val('');
-		$email.val('');
-		$subject.val('');
-		$message.val('');
+		})
+		.done(function(response) {
+			$button.removeClass('loading').prop('disabled', false);
+			$name.val('');
+			$email.val('');
+			$subject.val('');
+			$message.val('');
 
-		$('.status-message').addClass('hidden');
-		$('.status-message.text-success').removeClass('hidden');
-	})
-	.fail(function(response) {
-		$('.status-message').addClass('hidden');
-		$('.status-message.text-danger').removeClass('hidden');
+			$('.status-message').addClass('hidden');
+			$('.status-message.text-success').removeClass('hidden');
+		})
+		.fail(function(response) {
+			$('.status-message').addClass('hidden');
+			$('.status-message.text-danger').removeClass('hidden');
+		});
 	});
-});
+}
